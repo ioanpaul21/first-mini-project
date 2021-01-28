6d8f82b2f09f008cf57cc7ed2e54f7d0e4e3d517
ioanpaul21 <paul180913@gmail.com>
2021-01-28 13:29:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const random = Math.floor(Math.random() * 3);
+
+class App extends Component {
+  refreshPage() {
+    window.location.reload();
+  }
+
+  createButtons = () => {
+    const buttons = [];
+    for (let i = 0; i < 3; ++i) {
+      buttons.push(
+        <button
+          className="button"
+          key={i}
+          onClick={() =>
+            Number(buttons[i].key) === random
+              ? alert("Correct!")
+              : alert("Try another one!")
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Try me
+        </button>
+      );
+    }
+    return buttons;
+  };
+  render() {
+    return (
+      <div className="div_game">
+        <h1>Guess the button</h1>
+        <h2>Have fun!</h2>
+        {this.createButtons()}
+        <div>
+          <button className="button" onClick={this.refreshPage}>
+            Start Again
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
